619d4bb6f3d7751e20b024f27012f35f40812e9d
anaverage-enri <ibenrique2510@gmail.com>
2021-09-25 22:30:16 +0700

    Format for better convention and update mobile message

--- js/hbar-chart.js
+++ js/hbar-chart.js
@@ -1,9 +1,13 @@
 const len = casesData.CurrentlyInfected.length;
-const data = [casesData.CurrentlyInfected[len-1][1],
-              casesData.Recoveries[len-1][1],
-              casesData.Deaths[len-1][1]];
+const data = [
+  casesData.CurrentlyInfected[len - 1][1],
+  casesData.Recoveries[len - 1][1],
+  casesData.Deaths[len - 1][1],
+];
 
-const max = data.reduce(function(a, b) { return a + b; }, 0);
+const max = data.reduce(function (a, b) {
+  return a + b;
+}, 0);
 
 const max_format_1 = max.toString().slice(-3);
 const max_format_2 = max.toString().slice(-6, -3);
@@ -13,21 +17,27 @@ const max_format = max_format_3 + " " + max_format_2 + " " + max_format_1;
 const data_0_format_1 = data[0].toString().slice(-3);
 const data_0_format_2 = data[0].toString().slice(-6, -3);
 const data_0_format_3 = data[0].toString().slice(-9, -6);
-const data_0_format = data_0_format_3 + " " + data_0_format_2 + " " + data_0_format_1;
+const data_0_format =
+  data_0_format_3 + " " + data_0_format_2 + " " + data_0_format_1;
 
 const data_1_format_1 = data[1].toString().slice(-3);
 const data_1_format_2 = data[1].toString().slice(-6, -3);
 const data_1_format_3 = data[1].toString().slice(-9, -6);
-const data_1_format = data_1_format_3 + " " + data_1_format_2 + " " + data_1_format_1;
+const data_1_format =
+  data_1_format_3 + " " + data_1_format_2 + " " + data_1_format_1;
 
 const data_2_format_1 = data[2].toString().slice(-3);
 const data_2_format_2 = data[2].toString().slice(-6, -3);
 const data_2_format_3 = data[2].toString().slice(-9, -6);
-const data_2_format = data_2_format_3 + " " + data_2_format_2 + " " + data_2_format_1;
+const data_2_format =
+  data_2_format_3 + " " + data_2_format_2 + " " + data_2_format_1;
 
-document.getElementById("hbar-current-cases-value").innerHTML = data_0_format + " ca";
-document.getElementById("hbar-live-cases-value").innerHTML = data_1_format + " ca";
-document.getElementById("hbar-dead-cases-value").innerHTML = data_2_format + " ca";
+document.getElementById("hbar-current-cases-value").innerHTML =
+  data_0_format + " ca";
+document.getElementById("hbar-live-cases-value").innerHTML =
+  data_1_format + " ca";
+document.getElementById("hbar-dead-cases-value").innerHTML =
+  data_2_format + " ca";
 document.getElementById("total-data").innerHTML = max_format;
 
 const INACTIVE_COL = "#202124";
@@ -39,14 +49,19 @@ const current_bar = document.getElementById("hbar-current-cases");
 const live_bar = document.getElementById("hbar-live-cases");
 const dead_bar = document.getElementById("hbar-dead-cases");
 
-current_bar.style.background = `linear-gradient(to right, ` +
-  `${ACTIVE_CURRENT_COL} 0%, ${ACTIVE_CURRENT_COL} ${data[0]/max*100}%, ` +
-  `${INACTIVE_COL} ${data[0]/max*100}%, ${INACTIVE_COL} 100%)`;
+current_bar.style.background =
+  `linear-gradient(to right, ` +
+  `${ACTIVE_CURRENT_COL} 0%, ${ACTIVE_CURRENT_COL} ${
+    (data[0] / max) * 100
+  }%, ` +
+  `${INACTIVE_COL} ${(data[0] / max) * 100}%, ${INACTIVE_COL} 100%)`;
 
-live_bar.style.background = `linear-gradient(to right, ` +
-  `${ACTIVE_LIVE_COL} 0%, ${ACTIVE_LIVE_COL} ${data[1]/max*100}%, ` +
-  `${INACTIVE_COL} ${data[1]/max*100}%, ${INACTIVE_COL} 100%)`;
+live_bar.style.background =
+  `linear-gradient(to right, ` +
+  `${ACTIVE_LIVE_COL} 0%, ${ACTIVE_LIVE_COL} ${(data[1] / max) * 100}%, ` +
+  `${INACTIVE_COL} ${(data[1] / max) * 100}%, ${INACTIVE_COL} 100%)`;
 
-dead_bar.style.background = `linear-gradient(to right, ` +
-  `${ACTIVE_DEAD_COL} 0%, ${ACTIVE_DEAD_COL} ${data[2]/max*100}%, ` +
-  `${INACTIVE_COL} ${data[2]/max*100}%, ${INACTIVE_COL} 100%)`;
+dead_bar.style.background =
+  `linear-gradient(to right, ` +
+  `${ACTIVE_DEAD_COL} 0%, ${ACTIVE_DEAD_COL} ${(data[2] / max) * 100}%, ` +
+  `${INACTIVE_COL} ${(data[2] / max) * 100}%, ${INACTIVE_COL} 100%)`;
--- js/map-chart.js
+++ js/map-chart.js
@@ -1,68 +1,76 @@
 // ============= MAP =================
 // https://api.highcharts.com/highmaps
-Highcharts.mapChart('map-chart-container', {
-
+Highcharts.mapChart("map-chart-container", {
   chart: {
-    map: 'countries/vn/vn-all',
-    backgroundColor: 'black',
+    map: "countries/vn/vn-all",
+    backgroundColor: "black",
   },
-  mapNavigation: { /* enabled: true, */},
-  series: [{
-    data: mapData.data,
-    joinBy: ['iso_3166_2', 'iso3166_2'],
-    states: {
-      hover: {
-        // borderColor: 'red',
-        // brightness: -.5,
-        // color: 'rgba(219, 26, 26, 0.3)'
+  mapNavigation: {
+    /* enabled: true, */
+  },
+  series: [
+    {
+      data: mapData.data,
+      joinBy: ["iso_3166_2", "iso3166_2"],
+      states: {
+        hover: {
+          // borderColor: 'red',
+          // brightness: -.5,
+          // color: 'rgba(219, 26, 26, 0.3)'
+        },
       },
+      borderColor: "black",
     },
-    borderColor: 'black'
-  }],
-  
-  title: {text: ''},
+  ],
+
+  title: { text: "" },
   colorAxis: {
     endOnTick: false,
     reversed: false,
     min: 0,
     max: 1,
     stops: [
-      [0, 'rgb(255, 235, 235)'],
-      [1/4, 'rgb(244, 164, 164)'],
-      [2/4, 'rgb(235, 87, 87)'],
-      [3/4, 'rgb(219, 26, 26)'],
-      [1, 'rgb(146, 17, 17)'],
+      [0, "rgb(255, 235, 235)"],
+      [1 / 4, "rgb(244, 164, 164)"],
+      [2 / 4, "rgb(235, 87, 87)"],
+      [3 / 4, "rgb(219, 26, 26)"],
+      [1, "rgb(146, 17, 17)"],
     ],
     labels: {
-      style: {color: 'white'},
-      formatter: function(){return this.value + (this.isLast ? '+' : '')}
+      style: { color: "white" },
+      formatter: function () {
+        return this.value + (this.isLast ? "+" : "");
+      },
     },
   },
   legend: {
     // symbolWidth: 300,
-    layout: 'vertical',
-    align: 'left',
-    verticalAlign: 'bottom',
+    layout: "vertical",
+    align: "left",
+    verticalAlign: "bottom",
     floating: true,
   },
-  credits: {enabled: false},
-  
+  credits: { enabled: false },
+
   tooltip: {
-    backgroundColor: 'rgb(235, 87, 87)',
+    backgroundColor: "rgb(235, 87, 87)",
     borderRadius: 10,
     borderWidth: 0,
     shadow: false,
-    style: {color: 'white'},
+    style: { color: "white" },
     useHTML: true,
-    formatter: function() {
+    formatter: function () {
       let text = '<div class="tooltip">';
       text += `<div class="tooltip-name">${this.point.vName}</div>`;
-      text += '<div class="tooltip-info-column">Ca nhiễm:&nbsp;<br>Tử vong:&nbsp;</div>';
-      text += `<div class="tooltip-info-column">${addSeparator(this.point.cases, ' ')}<br>`;
-      text +=                                  `${addSeparator(this.point.deaths, ' ')}</div>`;
-      text += '</div>';
+      text +=
+        '<div class="tooltip-info-column">Ca nhiễm:&nbsp;<br>Tử vong:&nbsp;</div>';
+      text += `<div class="tooltip-info-column">${addSeparator(
+        this.point.cases,
+        " "
+      )}<br>`;
+      text += `${addSeparator(this.point.deaths, " ")}</div>`;
+      text += "</div>";
       return text;
     },
-  }
-
+  },
 });
